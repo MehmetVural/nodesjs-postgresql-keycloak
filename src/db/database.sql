ed32317c5d4eee9227d1066bb92c9a230e39c464
Mehmet Vural <mehmetalivural@yahoo.com>
2020-12-13 16:27:57 -0800

    keycloak protect

--- src/db/database.sql
+++ src/db/database.sql
@@ -12,18 +12,18 @@ CREATE USER youruser WITH ENCRYPTED PASSWORD 'yourpass';
 GRANT ALL PRIVILEGES ON DATABASE yourdbname TO youruser;
 
 -- Crete Table
-DROP TABLE pages;
-CREATE TABLE pages (
+DROP TABLE workbc.pages;
+CREATE TABLE workbc.pages (
 	page_id serial PRIMARY KEY,
 	page_title VARCHAR ( 50 ) NOT NULL,
 	page_desc VARCHAR ( 255 ) NOT NULL,
-	--created_on TIMESTAMP NOT NULL,
-    --updated_on TIMESTAMP NOT NULL,
     created_by INT
 );
+	--created_on TIMESTAMP NOT NULL,
+    --updated_on TIMESTAMP NOT NULL,
 -- insert some values
- -- INSERT INTO pages(page_title, page_desc) VALUES ('Title1', 'Desc') RETURNING *; INSERT INTO pages(page_title, page_desc) VALUES ('Title2', 'Desc') RETURNING *; INSERT INTO pages(page_title, page_desc) VALUES ('Title3', 'Desc') RETURNING *;
-INSERT INTO pages(page_title, page_desc) VALUES ('Title1', 'Desc'), ('Title2', 'Desc'), ('Title3', 'Desc');
+--INSERT INTO pages(page_title, page_desc) VALUES ('Title1', 'Desc') RETURNING *; INSERT INTO pages(page_title, page_desc) VALUES ('Title2', 'Desc') RETURNING *; INSERT INTO pages(page_title, page_desc) VALUES ('Title3', 'Desc') RETURNING *;
+INSERT INTO pages(page_title, page_desc) VALUES ('Title4', 'Desc'), ('Title5', 'Desc'), ('Title6', 'Desc');
 
 SELECT * FROm pages
 
